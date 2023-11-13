f2e0b7ed99ed50d0bcfe6aaecd1bcbc4f1cb6df6
Deepak <rohit45deepak@gmail.com>
2023-11-13 16:21:08 +0530

    added packeagfe

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     author="Deepak Gautam",
     author_email="rohit45deepak@gmail.com",
     url="https://www.sddoc.in",
+    
     description="A Python package for retrieving vehicle information, including challans and additional details.",
+    long_description="This package provides a Python interface for retrieving information about vehicles, including details about challans and additional information. check docs in https://github.com/deepakstark01/getvehicledata ",
+    long_description_content_type="text/plain",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
